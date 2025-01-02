e81ec11750d3adebc8a89f0de40b8fc64a951f5a
bhanuprakashsfdc <bhanusunny7@gmail.com>
2025-01-02 14:05:24 +0530

    fixing bug testing

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Services from '../components/Services/Services'
+import ServicesCmp from '../components/Services/Services'
 const ServicesPage = () => {
   return (
     <div>
-      <Services />
+      <ServicesCmp />
     </div>
   )
 }
